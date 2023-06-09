40a074f6b03c0c0ddf3316df847c07ab4fa2c501
kenn0808 <kenkwando08@gmail.com>
2023-06-09 23:02:55 +0700

    160420050_Kenny_ANMP_Week 9

---
+++ app/src/main/java/com/kenny/a160420050_todoapp/view/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.kenny.a160420050_todoapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.kenny.a160420050_todoapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.fragmentHost) as NavHostFragment).navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+
+    }
+}
